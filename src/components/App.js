6d6435472b34e17621544427a5cf4cf3e1c54552
nehapriya03 <56948455+nehapriya03@users.noreply.github.com>
2021-12-20 15:28:55 -0500

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+
+const App = () => {
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Header></Header>
+          <Routes>
+            <Route path="/" element={<StreamList />} />
+            <Route path="/streams/new" element={<StreamCreate />} />
+            <Route path="/streams/edit" element={<StreamEdit />} />
+            <Route path="/streams/delete" element={<StreamDelete />} />
+            <Route path="/streams/show" element={<StreamShow />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
